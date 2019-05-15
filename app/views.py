c25da4ea9287a9e65b11977e65f854f85bc8d350
haksoat <shopejuh@gmail.com>
2019-05-15 14:37:15 +0100

    Product movement added

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from app import app, models
 from flask import request
-from app.forms import AddForm
+from app.forms import AddForm, ProductMovementForm
 
 
 @app.route("/")
@@ -15,7 +15,7 @@ def products():
 
 @app.route("/add_product", methods=["GET", "POST"])
 def add_product():
-    form = AddForm()
+    form = AddForm(form_name="Add Product")
     if request.method == "POST" and form.validate_on_submit():
         product = models.Product(name=form.name.data, description=form.description.data)
         models.db.session.add(product)
@@ -30,7 +30,7 @@ def locations():
 
 @app.route("/add_location", methods=["GET", "POST"])
 def add_location():
-    form = AddForm()
+    form = AddForm(form_name="Add Location")
     if request.method == "POST" and form.validate_on_submit():
         location = models.Location(name=form.name.data, description=form.description.data)
         models.db.session.add(location)
@@ -38,6 +38,15 @@ def add_location():
     return "Add Location"
 
 
-@app.route("/movements")
-def movements():
-    return "Movements"
+@app.route("/product_movement", methods=["GET", "POST"])
+def product_movement():
+    form = ProductMovementForm(form_name="Product Movement")
+    form.product.choices = [(product.id, product.name) for product in models.Product.query.all()]
+    form.from_location.choices = [(location.id, location.name) for location in models.Location.query.all()]
+    form.to_location.choices = [(location.id, location.name) for location in models.Location.query.all()]
+    if request.method == "POST" and form.validate_on_submit():
+        movement = models.ProductMovement(from_location=form.from_location.data, to_location=form.to_location.data,
+                                          product_id=form.product.data, qty=form.qty.data)
+        models.db.session.add(movement)
+        models.db.session.commit()
+    return "Add Location"
