39f4453827723e4639cc8937764c2fa7f6e3bb43
haksoat <shopejuh@gmail.com>
2019-05-15 14:36:48 +0100

    Form edited

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,19 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import HiddenField, IntegerField, SelectField, StringField, SubmitField
 from wtforms.validators import DataRequired
 
 
 class AddForm(FlaskForm):
+    form_name = HiddenField("Form Name")
     name = StringField("Name", validators=[DataRequired()])
     description = StringField("Description", validators=[DataRequired()])
     submit = SubmitField("Add")
+
+
+class ProductMovementForm(FlaskForm):
+    form_name = HiddenField('Form Name')
+    product = SelectField('Product:', coerce=int, validators=[DataRequired()])
+    from_location = SelectField('From:', coerce=int)
+    to_location = SelectField('To:', coerce=int)
+    qty = IntegerField("Quantity")
+    submit = SubmitField('Move Product')
