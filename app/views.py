61eadbddacc3c9aa46edd369dcee7fd8cc4cb9bb
haksoat <shopejuh@gmail.com>
2019-05-15 09:48:09 +0100

    Logic to add location and product

--- app/views.py
+++ app/views.py
@@ -1,4 +1,6 @@
-from app import app
+from app import app, models
+from flask import request
+from app.forms import AddForm
 
 
 @app.route("/")
@@ -11,9 +13,29 @@ def products():
     return "Products"
 
 
+@app.route("/add_product", methods=["GET", "POST"])
+def add_product():
+    form = AddForm()
+    if request.method == "POST" and form.validate_on_submit():
+        product = models.Product(name=form.name.data, description=form.description.data)
+        models.db.session.add(product)
+        models.db.session.commit()
+    return "Add Product"
+
+
 @app.route("/locations")
 def locations():
     return "Locations"
+
+
+@app.route("/add_location", methods=["GET", "POST"])
+def add_location():
+    form = AddForm()
+    if request.method == "POST" and form.validate_on_submit():
+        location = models.Location(name=form.name.data, description=form.description.data)
+        models.db.session.add(location)
+        models.db.session.commit()
+    return "Add Location"
 
 
 @app.route("/movements")
