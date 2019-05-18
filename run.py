317af2ecafe1a375958a24dd3f094cf8b55796ed
haksoat <shopejuh@gmail.com>
2019-05-18 01:55:29 +0100

    Set up for running app

---
+++ run.py
@@ -0,0 +1,21 @@
+from app import app, models
+import config
+import sqlalchemy as sa
+
+
+def set_up():
+    app.config.from_object(config.DevConfig)
+    with app.app_context():
+        models.db.init_app(app)
+        models.db.create_all()
+        try:
+            location = models.Location(name="Abroad", description="Not a warehouse")
+            models.db.session.add(location)
+            models.db.session.commit()
+        except sa.exc.IntegrityError:
+            pass
+    app.run()
+
+
+if __name__ == "__main__":
+    set_up()
